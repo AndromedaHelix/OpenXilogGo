52a53d8c76760ab3650a624ef4b103e70a65e8a8
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-08 14:09:01 -0600

    Fixed #1 by finishing file

--- api/apimanager.js
+++ api/apimanager.js
@@ -1,4 +1,5 @@
 // Written by Juan Pablo Gutiérrez
+// Manages all server side fetching
 
 require('dotenv').config({
     path: ".env"
@@ -19,6 +20,8 @@ async function getJSONData(url) {
 
 /**
  * Returns array of loggers for the access token
+ * 
+ * @returns An array of loggers containing serial number and name.
  */
 function getAllData() {
     const relativePath = `${path}Logger/All/${API_KEY}`;
@@ -32,12 +35,37 @@ function getAllData() {
  * @param {String} serialNumber Serial number as displayed on device
  * @param {String} startDate Date at which to start querying loggers channel data (yyyy-MM-dd HH:mm)
  * @param {String} endDate Date at which to finish querying loggers channel data. (No more than 8 days after start date) (yyyy-MM-dd HH:mm)
- * @returns 
+ * 
+ * @returns An object which contains an array of channels each containing an array of the channel data:
  */
 function getLoggerData(serialNumber, startDate, endDate) {
-    const relativePath = `${path}/Data/All/${serialNumber}/${startDate}/${endDate}/${API_KEY}`;
+    const relativePath = `${path}Data/All/${serialNumber}/${startDate}/${endDate}/${API_KEY}`;
 
     return getJSONData(relativePath);
 }
 
-getAllData().then(out => console.log(out));
+/**
+ * Returns a collection of the raw data for the specified Xilog NG logger and channel name. (Max 8 days)
+ * 
+ * @param {Number} serialNumber Serial number as displayed on device
+ * @param {String} startDate Date at which to start querying loggers channel data (yyyy-MM-dd HH:mm)
+ * @param {String} endDate Date at which to finish querying loggers channel data (No more than 8 days after start date) (yyyy-MM-dd HH:mm)
+ * @param {String} channelName Name of the channel to retrieve data from.
+ * 
+ * @returns An object which contains an array of the loggers channel data: 
+ */
+function getChannelData(serialNumber, startDate, endDate, channelName) {
+    const relativePath = `${path}Data/Channel/${serialNumber}/${channelName}/${startDate}/${endDate}/${API_KEY}`;
+
+    return getJSONData(relativePath);
+}
+
+
+function getDailyData(serialNumber, startDate, endDate) {
+    const relativePath = `${path}Data/DailyStats/${serialNumber}/${startDate}/${endDate}/${API_KEY}`;
+
+    console.log(relativePath);
+    return getJSONData(relativePath);
+}
+
+/* getDailyData(2304402, "2023-06-04", "2023-06-08").then(out => console.log(out)) */
