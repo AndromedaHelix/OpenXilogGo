ab1550bf025929266b74d6dcc83aefe865bcf298
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-08 12:02:43 -0600

    Changed apimanager.js

--- api/apimanager.js
+++ api/apimanager.js
@@ -4,6 +4,6 @@ require('dotenv').config({
     path: ".env"
 })
 
-console.log("Hii " + process.env.API_KEY)
+console.log("API VALUE: " + process.env.API_KEY)
 
 const path = "https://xilogdataapi.atriumiot.com/"
