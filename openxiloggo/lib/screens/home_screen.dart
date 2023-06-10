d6eb4bf3262989ca3d337c6f0bbcdc8afb497891
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-10 08:22:59 -0600

    Created home screen

---
+++ openxiloggo/lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+/// Written by Juan Pablo Gutiérrez
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
