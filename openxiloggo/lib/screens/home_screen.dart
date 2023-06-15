b1aa43ed9b37e3137c0f672128bf383705474c42
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-15 13:54:54 -0600

    Added text

--- openxiloggo/lib/screens/home_screen.dart
+++ openxiloggo/lib/screens/home_screen.dart
@@ -1,12 +1,19 @@
 /// Written by Juan Pablo Gutiérrez
 
 import 'package:flutter/material.dart';
+import 'package:openxiloggo/widgets/BigText.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: BigText(
+          titleText: "OpenXiLogGo",
+        ),
+      ),
+    );
   }
 }
