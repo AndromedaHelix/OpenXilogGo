914703c9bcc258d61750b461cac6c07edca104d5
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-18 21:01:56 -0600

    Fixed #6 API not working

--- apimanager.dart
+++ apimanager.dart
@@ -1,13 +1,12 @@
 /// Written by Juan Pablo Gutiérrez
 /// 17 - 06 - 2023
 ///
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String sourcePath = "https://xilogdataapi.atriumiot.com/2";
 
 Future main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
 
-  /* print(dotenv.env['API_KEY']); */
+  print(dotenv.get("API_KEY"));
 }
