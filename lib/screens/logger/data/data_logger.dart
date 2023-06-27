91f8467a0f07842f54f487573e00e0ca0f4dbf9d
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-26 18:43:49 -0600

    Added calendar

--- lib/screens/logger/data/data_logger.dart
+++ lib/screens/logger/data/data_logger.dart
@@ -3,12 +3,14 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_date_range_picker/flutter_date_range_picker.dart';
 
 import '../../../widgets/back_icon.dart';
 import '../../../widgets/big_text.dart';
 
 class DataLoggerPage extends StatelessWidget {
   final String serialNumber;
+
   const DataLoggerPage({required this.serialNumber, super.key});
 
   @override
@@ -23,19 +25,36 @@ class DataLoggerPage extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    BackIcon(color: Colors.black),
+                    const BackIcon(color: Colors.black),
                     Text(serialNumber),
                   ],
                 ),
                 const BigText(titleText: "Select date range"),
-                CalendarDatePicker(
+                DateRangeField(
+                  pickerBuilder: (context, onDateRangeChanged) {
+                    return SingleChildScrollView(
+                      child: DateRangePickerWidget(
+                        doubleMonth: false,
+                        maximumDateRangeLength: 8,
+                        minimumDateRangeLength: 1,
+                        initialDateRange:
+                            DateRange(DateTime(2022), DateTime(2023)),
+                        disabledDates: [DateTime(2023, 11, 20)],
+                        initialDisplayedDate: DateTime.now(),
+                        onDateRangeChanged: (value) => print(value),
+                      ),
+                    );
+                  },
+                ),
+
+                /* CalendarDatePicker(
                   initialDate: DateTime.now(),
                   firstDate: new DateTime(2022),
                   lastDate: new DateTime(2100),
                   onDateChanged: (value) =>
                       print("${value.year}-${value.month}-${value.day}"),
-                ),
-                SizedBox(
+                ), */
+                /* SizedBox(
                   height: MediaQuery.of(context).size.height / 5.5,
                   width: MediaQuery.of(context).size.width / 2,
                   child: CupertinoDatePicker(
@@ -44,7 +63,7 @@ class DataLoggerPage extends StatelessWidget {
                     use24hFormat: true,
                     mode: CupertinoDatePickerMode.time,
                   ),
-                ),
+                ), */
               ],
             ),
           ),
