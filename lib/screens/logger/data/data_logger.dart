c3162e593e2d303acace0781fbf69c56c9b27601
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-27 22:49:55 -0600

    Removed bulky code from data_logger.dart

--- lib/screens/logger/data/data_logger.dart
+++ lib/screens/logger/data/data_logger.dart
@@ -1,18 +1,14 @@
 /// Written by Juan Pablo Gutiérrez
 /// 24 - 06 - 2023
 
-import 'package:flutter/cupertino.dart';
+import 'package:OpenXilogGo/widgets/date_time_selector.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_date_range_picker/flutter_date_range_picker.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../../../widgets/back_icon.dart';
-import '../../../widgets/big_text.dart';
 
 class DataLoggerPage extends StatefulWidget {
   final String serialNumber;
-
-  late DateRange dataRange =
-      DateRange(DateTime.now(), DateTime.now().add(const Duration(days: 1)));
 
   DataLoggerPage({required this.serialNumber, super.key});
 
@@ -21,42 +17,42 @@ class DataLoggerPage extends StatefulWidget {
 }
 
 class _DataLoggerPageState extends State<DataLoggerPage> {
+  PanelController controller = PanelController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const BackIcon(color: Colors.black),
-                    Text(widget.serialNumber),
-                  ],
-                ),
-                const BigText(titleText: "Select date range"),
-                DateRangeField(
-                  decoration: InputDecoration(labelText: "${widget.dataRange}"),
-                  pickerBuilder: (context, onDateRangeChanged) {
-                    return DateRangePickerWidget(
-                      doubleMonth: false,
-                      maximumDateRangeLength: 8,
-                      minimumDateRangeLength: 1,
-                      initialDateRange:
-                          DateRange(DateTime(2022), DateTime(2023)),
-                      disabledDates: [DateTime(2023, 11, 20)],
-                      initialDisplayedDate: DateTime.now(),
-                      onDateRangeChanged: (value) => setState(() {
-                        widget.dataRange = value!;
-                      }),
-                      height: 340,
-                    );
-                  },
-                ),
-              ],
+      body: SlidingUpPanel(
+        controller: controller,
+        borderRadius: BorderRadius.circular(30),
+        maxHeight: MediaQuery.of(context).size.height / 1.2,
+        backdropEnabled: true,
+        panel: Center(
+          child: Container(
+            child: Text("aa"),
+          ),
+        ),
+        body: Center(
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const BackIcon(),
+                      Text(widget.serialNumber),
+                    ],
+                  ),
+                  DateTimeSelector(
+                    serialNumber: widget.serialNumber,
+                    requestType: RequestType.dateTime,
+                    controller: controller,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
