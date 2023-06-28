26fd5b171b2c15ab7d15387c603cf265add224a3
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-06-27 22:48:56 -0600

    Created constants file

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+/// Written by Juan Pablo Gutiérrez
+/// 27 - 06 - 2023
+
+import 'package:flutter/cupertino.dart';
+
+TextStyle defaultStyle = const TextStyle(
+  fontFamily: "Manrope",
+  fontSize: 18,
+);
+
+TextStyle loggeInfoStyle = const TextStyle(
+    fontSize: 24, fontWeight: FontWeight.bold, fontFamily: "Manrope");
+
+TextStyle bigTextStyle = const TextStyle(
+    fontFamily: "Manrope",
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+    overflow: TextOverflow.visible);
